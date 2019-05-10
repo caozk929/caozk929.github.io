47cdd233a45165566bbbf126d6f19236171b74eb
caozk929 <caozk929@163.com>
2019-05-10 17:40:29 +0800

    我和宝宝

--- index_files/functions.js
+++ index_files/functions.js
@@ -41,17 +41,14 @@ function timeElapse(date){
 	var current = Date();
 	var seconds = (current - date) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
-	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	seconds = seconds % 60;
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
